b170ea936beeb6851094328674b25f5296df8bde
Yash Raj <yashrajan07@gmail.com>
2026-07-18 21:04:39 +0530

    Change TextAggregationMode back to SENTENCE for natural audio generation

--- services/tts/elevenlabs.py
+++ services/tts/elevenlabs.py
@@ -30,7 +30,7 @@ def create_elevenlabs_tts_service() -> ElevenLabsTTSService:
         api_key=ELEVENLABS_API_KEY,
         sample_rate=AUDIO_OUT_SAMPLE_RATE,
         settings=settings,
-        text_aggregation_mode=TextAggregationMode.TOKEN
+        text_aggregation_mode=TextAggregationMode.SENTENCE
     )
 
     return tts
